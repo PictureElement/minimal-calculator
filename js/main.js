6d709d75e9b978a1e2e6330f1280e201ac65c24f
Marios Sofokleous <marios.sofokleous@yandex.com>
2017-11-30 15:30:49 +0200

    Assign click event to the divide button

--- js/main.js
+++ js/main.js
@@ -131,12 +131,12 @@ add.on('click', function() {
     var currentValue = display2.val();
     
     // Example:
-    // currentValue = "2_x_3_-_4_-_"
+    // currentValue = "2_×_3_-_4_-_"
     // length = 12
     // indexOfLastVissibleChar = length - 2 = 10
     // lastVissibleChar = '-'
-    // str = "2_x_3_-_4"
-    // currentValue = "2_x_3_-_4_+_"
+    // str = "2_×_3_-_4"
+    // currentValue = "2_×_3_-_4_+_"
     var indexOfLastVissibleChar = currentValue.length - 2;
     var lastVissibleChar = currentValue.charAt(indexOfLastVissibleChar);
     var str;
@@ -173,12 +173,12 @@ subtract.on('click', function() {
     var currentValue = display2.val();
     
     // Example:
-    // currentValue = "2_x_3_-_4_+_"
+    // currentValue = "2_×_3_-_4_+_"
     // length = 12
     // indexOfLastVissibleChar = length - 2 = 10
     // lastVissibleChar = '+'
-    // str = "2_x_3_-_4"
-    // currentValue = "2_x_3_-_4_-_"
+    // str = "2_×_3_-_4"
+    // currentValue = "2_×_3_-_4_-_"
     var indexOfLastVissibleChar = currentValue.length - 2;
     var lastVissibleChar = currentValue.charAt(indexOfLastVissibleChar);
     var str;
@@ -215,12 +215,12 @@ multiply.on('click', function() {
     var currentValue = display2.val();
     
     // Example:
-    // currentValue = "2_x_3_-_4_+_"
+    // currentValue = "2_×_3_-_4_+_"
     // length = 12
     // indexOfLastVissibleChar = length - 2 = 10
     // lastVissibleChar = '+'
-    // str = "2_x_3_-_4"
-    // currentValue = "2_x_3_-_4_x_"
+    // str = "2_×_3_-_4"
+    // currentValue = "2_×_3_-_4_×_"
     var indexOfLastVissibleChar = currentValue.length - 2;
     var lastVissibleChar = currentValue.charAt(indexOfLastVissibleChar);
     var str;
@@ -253,10 +253,43 @@ multiply.on('click', function() {
 })
 
 divide.on('click', function() {
+    
     var currentValue = display2.val();
-    display2.val(currentValue + ' \u00F7 ');
+    
+    // Example:
+    // currentValue = "2_×_3_-_4_+_"
+    // length = 12
+    // indexOfLastVissibleChar = length - 2 = 10
+    // lastVissibleChar = '+'
+    // str = "2_×_3_-_4"
+    // currentValue = "2_×_3_-_4_÷_"
+    var indexOfLastVissibleChar = currentValue.length - 2;
+    var lastVissibleChar = currentValue.charAt(indexOfLastVissibleChar);
+    var str;
+    
+    switch (lastVissibleChar) {
+        // Last visible character is the subtraction sign
+        case "\u2212":
+            str = currentValue.substr(0, indexOfLastVissibleChar - 1)
+            currentValue = str + ' \u00F7 ';
+            display2.val(currentValue);
+            break;
+        // Last vissible character is the multiplication sign
+        case "\u00D7":
+            str = currentValue.substr(0, indexOfLastVissibleChar - 1)
+            currentValue = str + ' \u00F7 ';
+            display2.val(currentValue);
+            break;
+        // Last vissible character is the division sign
+        case "\u00F7":
+            break;
+        // Last vissible character is addition sign
+        case "\u002B":
+            str = currentValue.substr(0, indexOfLastVissibleChar - 1)
+            currentValue = str + ' \u00F7 ';
+            display2.val(currentValue);
+            break;
+        default:
+            display2.val(currentValue + ' \u00F7 ');
+    }
 })
-
-
-
-
