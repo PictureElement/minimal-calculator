df993863fa13afb0a43baf1b0719452ffbcf5143
Marios Sofokleous <marios.sofokleous@yandex.com>
2017-12-20 14:05:15 +0200

    Update docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,14 +3,12 @@
 var display1 = {
     operation: "",
     evaluation: "",
-    answer: "",
-    unbalancedParentheses: 0, // Number of unbalanced parentheses
+    answer: ""
 };
 
 // default flag values
 var flag = {
     ansAllowed: false, // Initially do not allow the use of Ans button
-    squareRoot: false, // Square root evaluation
     /*
     decimalPointAllowed: true,
     pctAllowed: false,
@@ -76,132 +74,77 @@ function evaluate() {
 
 // Digits
 $('#zero').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "0" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "0";
-    }
+    display1.operation = display1.operation + "0";
     $('#display1').val($('#display1').val() + '\u0030');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#one').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "1" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "1";
-    }
+    display1.operation = display1.operation + "1";
     $('#display1').val($('#display1').val() + '\u0031');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#two').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "2" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "2";
-    }
+    display1.operation = display1.operation + "2";
     $('#display1').val($('#display1').val() + '\u0032');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#three').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "3" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "3";
-    }
+    display1.operation = display1.operation + "3";
     $('#display1').val($('#display1').val() + '\u0033');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#four').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "4" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "4";
-    }
+    display1.operation = display1.operation + "4";
     $('#display1').val($('#display1').val() + '\u0034');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#five').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "5" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "5";
-    }
+    display1.operation = display1.operation + "5";
     $('#display1').val($('#display1').val() + '\u0035');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#six').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "6" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "6";
-    }
+    display1.operation = display1.operation + "6";
     $('#display1').val($('#display1').val() + '\u0036');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#seven').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "7" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "7";
-    }
+    display1.operation = display1.operation + "7";
     $('#display1').val($('#display1').val() + '\u0037');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#eight').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "8" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "8";
-    }
+    display1.operation = display1.operation + "8";
     $('#display1').val($('#display1').val() + '\u0038');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#nine').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "9" + ")";
-    }
-    else {
-        display1.operation = display1.operation + "9";
-    }
+    display1.operation = display1.operation + "9";
     $('#display1').val($('#display1').val() + '\u0039');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
               
 $('#decimal').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "." + ")";
-    }
-    else {
-        display1.operation = display1.operation + ".";
-    }
+    display1.operation = display1.operation + ".";
     $('#display1').val($('#display1').val() + '\u002e');
     evaluate();
     $('#display2').val(display1.evaluation);
@@ -209,38 +152,20 @@ $('#decimal').on('click', function () {
 
 // Operators
 $('#left-parenthesis').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + "(" + ")";
-        display1.unbalancedParentheses ++;
-    }
-    else {
-        display1.operation = display1.operation + "(";
-    }
+    display1.operation = display1.operation + "(";
     $('#display1').val($('#display1').val() + '\u0028');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#right-parenthesis').on('click', function () {
-    if (flag.squareRoot) {
-        display1.operation = display1.operation.substring(0, display1.operation.length-1) + ")" + ")";
-        display1.unbalancedParentheses --;
-        if (flag.unbalancedParentheses <= 0) {
-            display1.squareRoot = false;
-        }
-    }
-    else {
-        display1.operation = display1.operation + ")";
-    }
+    display1.operation = display1.operation + ")";
     $('#display1').val($('#display1').val() + '\u0029');
     evaluate();
     $('#display2').val(display1.evaluation);
 })
 
 $('#add').on('click', function () {
-    if (flag.squareRoot) {
-        flag.squareRoot = false;
-    }
     display1.operation = display1.operation + "+";
     $('#display1').val($('#display1').val() + '\u002b');
     evaluate();
@@ -248,9 +173,6 @@ $('#add').on('click', function () {
 })
 
 $('#subtract').on('click', function () {
-    if (flag.squareRoot) {
-        flag.squareRoot = false;
-    }
     display1.operation = display1.operation + "-";
     $('#display1').val($('#display1').val() + '\u2212');
     evaluate();
@@ -258,9 +180,6 @@ $('#subtract').on('click', function () {
 })
 
 $('#multiply').on('click', function () {
-    if (flag.squareRoot) {
-        flag.squareRoot = false;
-    }
     display1.operation = display1.operation + "*";
     $('#display1').val($('#display1').val() + '\u00d7');
     evaluate();
@@ -268,9 +187,6 @@ $('#multiply').on('click', function () {
 })
 
 $('#divide').on('click', function () {
-    if (flag.squareRoot) {
-        flag.squareRoot = false;
-    }
     display1.operation = display1.operation + "/";
     $('#display1').val($('#display1').val() + '\u00f7');
     evaluate();
@@ -278,15 +194,106 @@ $('#divide').on('click', function () {
 })
 
 $('#square-root').on('click', function () {
-    flag.squareRoot = true; // Raise the flag
-    display1.operation = display1.operation + "sqrt()";
-    $('#display1').val($('#display1').val() + '\u221a');
+    
+    var count = 0;
+    var index = -1;
+    var radicand = "";
+    var result = "";
+    
+    // radicand is a complex expression
+    if (display1.operation.endsWith(')')) 
+    {
+        // Find the position of matching parenthesis
+        // For example if operation="2+(3-(3+4))", then the index of the 
+        // matching '(' would be 2 and the radicand would be (3-(3+4))
+        for (i = display1.operation.length - 2; i > -1; i--) 
+        {
+            if (display1.operation.charAt(i) === '(') 
+            {
+                if (count === 0) 
+                {
+                    index = i;
+                    break;
+                } 
+                else 
+                {
+                    count -= 1;
+                    continue;
+                }
+            } 
+            else if (display1.operation.charAt(i) === ')') 
+            {
+                count += 1;
+            } 
+            else 
+            {
+                continue;
+            }
+        }
+        if (index === -1) 
+        {
+            //alert("Malformed expression");
+            $('#display2').val("Malformed expression");
+            return;
+        }
+    } 
+    // radicand is a single number
+    else if (display1.operation.endsWith('0') || display1.operation.endsWith('1') || 
+               display1.operation.endsWith('2') || display1.operation.endsWith('3') || 
+               display1.operation.endsWith('4') || display1.operation.endsWith('5') ||
+               display1.operation.endsWith('6') || display1.operation.endsWith('7') || 
+               display1.operation.endsWith('8') || display1.operation.endsWith('9') ||
+               display1.operation.endsWith('.'))
+    {
+        index = display1.operation.length - 1;
+        for (i = display1.operation.length - 2; i > -1; i--) 
+        {
+            if (display1.operation.charAt(i) === '0' || display1.operation.charAt(i) === '1' ||
+                display1.operation.charAt(i) === '2' || display1.operation.charAt(i) === '3' ||
+                display1.operation.charAt(i) === '4' || display1.operation.charAt(i) === '5' ||
+                display1.operation.charAt(i) === '6' || display1.operation.charAt(i) === '7' ||
+                display1.operation.charAt(i) === '8' || display1.operation.charAt(i) === '9' ||
+                display1.operation.charAt(i) === '.' || display1.operation.charAt(i) === '^')
+            {
+                index = i;
+            }
+            else 
+            {
+                break;
+            }
+        }
+    }
+    else 
+    {
+        return;
+    }
+    
+    // Get the radicand
+    radicand = display1.operation.substring(index, display1.operation.length);
+    
+    // Update operation with sqrt(radicand)
+    display1.operation = display1.operation.substring(0, index) + "sqrt(" + radicand + ")";
+    
+    // Replace '^2' with superscript two (don't forget to escape '^' with '\')
+    radicand = radicand.replace(/\^2/g, "\u00b2");
+    
+    // Find the last occurence of radicand
+    index = $('#display1').val().lastIndexOf(radicand);
+    
+    radicand = $('#display1').val().substring(index, $('#display1').val().length);
+    
+    // Update #display1
+    $('#display1').val($('#display1').val().substring(0, index) + '\u221a' + radicand);
+    
+    // Evaluate
     evaluate();
+    
+    // Display current evaluation
     $('#display2').val(display1.evaluation);
-})
-
+});
+        
 $('#square').on('click', function () {
-    display1.operation = display1.operation + "^(2)";
+    display1.operation = display1.operation + "^2";
     $('#display1').val($('#display1').val() + '\u00b2');
     evaluate();
     $('#display2').val(display1.evaluation);
@@ -348,8 +355,6 @@ $('#clear').on('click', function () {
     display1.evaluation = "",
     $('#display1').val("");
     $('#display2').val("");
-    display1.unbalancedParentheses = 0;
-    flag.squareRoot = false;
 })
 
 // Equal
@@ -358,9 +363,7 @@ $('#equal').on('click', function () {
     $('#display1').val(display1.answer); // Update display1
     $('#display2').val(""); // Update display2
     display1.operation = display1.answer; // Current operation equals the answer
-    display1.unbalancedParentheses = 0;
     flag.ansAllowed = true; // Allow the use of Ans button
-    flag.squareRoot = false;
 })
 
 $('#ans').on('click', function () {    
